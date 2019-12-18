59b4bdabffdfcc5257bfef576258ad3433a9f45c
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2019-12-18 13:37:42 +0900

    Tweak.

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -32,10 +32,10 @@ namespace WebApplication1.Controllers
         public IActionResult Privacy()
         {
         	string count = "";
-        	//// Docker Compose
-            //using (var con = new SqlConnection("Data Source=10.0.75.1\\sqlexpress;Initial Catalog=Northwind;User ID=sa;Password=seigi@123;"))
-            // Kubernetes
-            using (var con = new SqlConnection("Data Source=mssql-deployment;Initial Catalog=Northwind;User ID=sa;Password=MyC0m9l&xP@ssw0rd;"))            
+            // Docker Compose
+            using (var con = new SqlConnection("Data Source=10.0.75.1\\sqlexpress;Initial Catalog=Northwind;User ID=sa;Password=seigi@123;"))
+            //// Kubernetes
+            //using (var con = new SqlConnection("Data Source=mssql-deployment;Initial Catalog=Northwind;User ID=sa;Password=MyC0m9l&xP@ssw0rd;"))            
             {
                 con.Open();
                 var cmd = new SqlCommand(@"select count(*) from shippers", con);
